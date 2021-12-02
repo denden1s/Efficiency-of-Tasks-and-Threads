7c556616f9e0a705c8c179f6d619a2b89d02c49e
denden1s <den221001@gmail.com>
2021-12-02 11:03:11 +0300

    Finish job

---
+++ Functions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tasks
+{
+  class Functions
+  {
+    private List<Task> _tasks;
+    private List<Thread> _threads;
+    private Stopwatch timer;
+    private int _numForSearch;
+    private int _finaledCount;
+    public int _operationCount;
+    public int[] nums;
+
+    private void BinarySearch()
+    {
+      Array.BinarySearch(nums, _numForSearch);
+      _finaledCount++;
+    }
+    public Functions(int count, int[] nums)
+    {
+      _finaledCount = 0;
+      _operationCount = count;
+      this.nums = nums;
+      _threads = new List<Thread>();
+      _tasks = new List<Task>();
+      timer = new Stopwatch();
+      _numForSearch = nums[nums.Length - 1];
+      for(int i = 0; i < _operationCount; i++)
+      {
+        Thread th = new Thread(new ThreadStart(BinarySearch));
+        _threads.Add(th);
+        _tasks.Add(new Task(BinarySearch));
+      }
+    }
+    public void MakeTestsAsync(string type)
+    {
+      _finaledCount = 0;
+      if(type == "Task")
+      {
+        timer.Start();
+        foreach(Task t in _tasks)
+          t.RunSynchronously();
+
+        timer.Stop();
+        Console.WriteLine($"Кол-во выполненных задач: {_finaledCount}");
+      }
+      else if(type == "Thread")
+      {
+        timer.Start();
+        foreach(Thread th in _threads)
+          th.Start();
+
+        while(_finaledCount != _operationCount) { }
+        timer.Stop();
+        Console.WriteLine($"Кол-во выполненных потоков: {_finaledCount}");
+      }
+      Console.WriteLine($"Время выполнения {type}: {Math.Round((decimal)timer.ElapsedMilliseconds / 1000, 4)} c.");
+      timer.Reset();
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tasks
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] sortedNums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+      while(true)
+      {
+        Console.WriteLine("Введите количество повторений алгоритма:");
+        int count = Convert.ToInt32(Console.ReadLine());
+        Functions test = new Functions(count, sortedNums);
+        test.MakeTestsAsync("Task");
+        test.MakeTestsAsync("Thread");
+        Console.ReadKey();
+      }
+    }
+  }
+}
